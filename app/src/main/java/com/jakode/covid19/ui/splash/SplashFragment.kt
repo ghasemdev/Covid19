ffd6e484a24cad62a9b843567519c8ac719aee25
ghasem shirdel <shirdelghasem79@gmail.com>
2020-10-25 00:00:24 +0330

    add splash screen

---
+++ app/src/main/java/com/jakode/covid19/ui/splash/SplashFragment.kt
@@ -0,0 +1,57 @@
+package com.jakode.covid19.ui.splash
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.jakode.covid19.R
+import com.jakode.covid19.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSplashBinding.bind(view)
+        fullScreen() // Full screen size
+        navigate() // Navigate to other page
+    }
+
+    private fun navigate() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(5000)
+//            if (UserState(requireContext()).mode)
+//                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+//            else
+//                findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
+        }
+    }
+
+    private fun fullScreen() {
+        requireActivity().apply {
+            if (Build.VERSION.SDK_INT >= 30) {
+                window.setDecorFitsSystemWindows(false)
+                val controller: WindowInsetsController = window.insetsController!!
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            } else {
+                window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
