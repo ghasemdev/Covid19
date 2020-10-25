4173105c067d477d5bedf732eebb4990cac35e24
ghasem shirdel <shirdelghasem79@gmail.com>
2020-10-25 12:10:50 +0330

    add screens for intro

---
+++ app/src/main/java/com/jakode/covid19/ui/intro/Screens.kt
@@ -0,0 +1,8 @@
+package com.jakode.covid19.ui.intro
+
+import androidx.fragment.app.Fragment
+import com.jakode.covid19.R
+
+class WashYourHands : Fragment(R.layout.screen_wash_your_hand)
+class WearMask : Fragment(R.layout.screen_wear_mask)
+class UseNoseRag : Fragment(R.layout.screen_use_nose_rag)
