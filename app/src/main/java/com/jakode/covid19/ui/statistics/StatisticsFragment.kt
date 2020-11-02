a757e8352479a3c571596511f399cf70010916c4
ghasem shirdel <shirdelghasem79@gmail.com>
2020-11-03 01:08:33 +0330

    change animation of header in StatisticsFragment

--- app/src/main/java/com/jakode/covid19/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/jakode/covid19/ui/statistics/StatisticsFragment.kt
@@ -3,20 +3,17 @@ package com.jakode.covid19.ui.statistics
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.appbar.AppBarLayout
 import com.jakode.covid19.R
 import com.jakode.covid19.databinding.FragmentStatisticsBinding
 import com.jakode.covid19.model.Statistics
 import com.jakode.covid19.ui.MainActivity
 import com.jakode.covid19.ui.home.MainStateEvent
-import com.jakode.covid19.utils.AppBarStateChangeListener
 import com.jakode.covid19.utils.DataState
-import com.jakode.covid19.utils.popup.DetailsPopup
 import com.jakode.covid19.utils.OnBackPressedListener
+import com.jakode.covid19.utils.popup.DetailsPopup
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -34,7 +31,6 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics), OnBackPressed
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentStatisticsBinding.bind(view)
         observe()
-        parallax()
         toolbar()
         header()
 
@@ -43,62 +39,41 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics), OnBackPressed
     }
 
     private fun header() {
-        binding.toolbarHeader.apply {
-            backInHeader.setOnClickListener {
-                findNavController().navigateUp()
-            }
+        binding.header.apply {
+
         }
     }
 
-    @ExperimentalCoroutinesApi
     private fun toolbar() {
         // Set toolbar and menu
         binding.apply {
-            (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
-            toolbar.setNavigationOnClickListener {
+            toolbarBack.setOnClickListener {
                 findNavController().navigateUp()
             }
 
             toolbarMore.setOnClickListener { }
-            toolbarRefresh.setOnClickListener {
-                displayError(false)
-                viewModel.setStateEvent(MainStateEvent.GetBlogEvents, true)
-            }
+
             toolbarSearch.setOnClickListener { }
 
             toolbarMore.setOnLongClickListener {
-                popup(R.id.toolbar_more, 0, getString(R.string.more))
+                popup(R.id.toolbar_more, getString(R.string.more))
             }
-            toolbarRefresh.setOnLongClickListener {
-                popup(R.id.toolbar_refresh, -50, getString(R.string.refresh))
-            }
+
             toolbarSearch.setOnLongClickListener {
-                popup(R.id.toolbar_search, -50, getString(R.string.search))
+                popup(R.id.toolbar_search, getString(R.string.search))
             }
         }
     }
 
-    private fun popup(resource: Int, x: Int, massage: String): Boolean {
+    private fun popup(resource: Int, massage: String): Boolean {
         DetailsPopup().Builder {
             anchor = requireView().findViewById(resource)
             layoutResource = R.layout.details_popup_layout
-            coordinates.x = x
+            coordinates.x = -50
             elevation = 5F
             popupTexts = listOf(massage)
         }.build()
         return true
-    }
-
-    private fun parallax() {
-        binding.appbar.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
-            override fun onStateChanged(appBarLayout: AppBarLayout?, state: State?) {
-                binding.toolbar.visibility = when (state) {
-                    State.EXPANDED -> View.GONE
-                    State.COLLAPSED -> View.VISIBLE
-                    else -> View.GONE
-                }
-            }
-        })
     }
 
     @ExperimentalCoroutinesApi
@@ -112,7 +87,7 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics), OnBackPressed
                 }
                 is DataState.Error -> {
                     displayProgressBar(false)
-                    displayError(true, dataState.exception.message)
+                    displayError(dataState.exception.message)
                 }
                 is DataState.Loading -> {
                     displayProgressBar(true)
@@ -134,21 +109,21 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics), OnBackPressed
         }
     }
 
-    private fun displayError(isDisplayed: Boolean, massage: String? = "") {
+    private fun displayError(massage: String? = "") {
         if (massage != null)
             if (massage.isNotEmpty())
                 Log.e("ERROR", "displayError: $massage")
         binding.apply {
-            connectionError.visibility = if (isDisplayed) View.VISIBLE else View.GONE
-            connectionErrorText.visibility = if (isDisplayed) View.VISIBLE else View.GONE
-            nestedScrollView.visibility = if (isDisplayed) View.GONE else View.VISIBLE
+            connectionError.visibility = View.VISIBLE
+            connectionErrorText.visibility = View.VISIBLE
+//            nestedScrollView.visibility = View.GONE
         }
     }
 
     private fun displayProgressBar(isDisplayed: Boolean) {
         binding.apply {
             loading.visibility = if (isDisplayed) View.VISIBLE else View.GONE
-            nestedScrollView.visibility = if (isDisplayed) View.GONE else View.VISIBLE
+//            nestedScrollView.visibility = if (isDisplayed) View.GONE else View.VISIBLE
         }
     }
 
